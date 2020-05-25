b2af7b2d2e7bfd8aea54409d56755c1cfa19fd7a
DimitrianC <dimitrian.cebotaru@google.com>
2020-05-25 21:48:38 +0200

    DateList and GroupList :))

--- src/lists/DateList.java
+++ src/lists/DateList.java
@@ -1,5 +1,50 @@
 package lists;
 
-public class DateList
+import model.Date;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class DateList implements Serializable
 {
+  private ArrayList<Date> dates;
+
+  public DateList()
+  {
+    dates = new ArrayList<Date>();
+  }
+  public int size()
+  {
+    return dates.size();
+  }
+
+  public Date getDate(int index)
+  {
+    return dates.get(index);
+  }
+
+  public void addDate(Date date)
+  {
+    dates.add(date);
+  }
+  public void removeDate(Date date)
+  {
+    dates.remove(date);
+  }
+  public void removeDate(int index)
+  {
+    dates.remove(index);
+  }
+
+  @Override public String toString()
+  {
+    String returned = "Date: ";
+    for(int i=0;i<dates.size();i++)
+    {
+      returned+="\n"+dates.get(i).toString();
+    }
+    return returned;
+  }
+
+
 }
--- src/lists/GroupList.java
+++ src/lists/GroupList.java
@@ -1,5 +1,49 @@
 package lists;
 
-public class GroupList
+import model.Group;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class GroupList implements Serializable
 {
+  private ArrayList<Group> groups;
+
+  public GroupList()
+  {
+    groups = new ArrayList<Group>();
+  }
+  public int size()
+  {
+    return groups.size();
+  }
+
+  public Group getGroup(int index)
+  {
+    return groups.get(index);
+  }
+
+  public void addGroup(Group group)
+  {
+    groups.add(group);
+  }
+  public void removeGroup(Group group)
+  {
+    groups.remove(group);
+  }
+  public void removeGroup(int index)
+  {
+    groups.remove(index);
+  }
+
+  @Override public String toString()
+  {
+    String returned = "Groups: ";
+    for(int i=0;i<groups.size();i++)
+    {
+      returned+="\n"+groups.get(i).toString();
+    }
+    return returned;
+  }
+
 }
