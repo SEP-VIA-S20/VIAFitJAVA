8f31aef2e72febc978d2a736196db4d53fb9405f
DimitrianC <dimitrian.cebotaru@google.com>
2020-05-29 16:05:28 +0200

    Added Some FileAdapter :))

---
+++ src/saves/DateFileAdapter.java
@@ -0,0 +1,97 @@
+package saves;
+
+import lists.DateList;
+import model.Date;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class DateFileAdapter
+{
+  private String fileName;
+
+  public DateFileAdapter(String fileName)
+  {
+    this.fileName = fileName;
+  }
+
+  public String getFileName()
+  {
+    return fileName;
+  }
+
+  public void setFileName(String fileName)
+  {
+    this.fileName = fileName;
+  }
+
+  public void saveDates(DateList obj)
+  {
+    FileIO fileOut = new FileIO();
+    try
+    {
+      fileOut.writeToFile(fileName, obj);
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println("File not found");
+    }
+    catch (IOException e)
+    {
+      System.out.println("IO exception saving cars");
+    }
+  }
+
+  public DateList getAllDates()
+  {
+    FileIO fileIn = new FileIO();
+    Object obj = null;
+    try
+    {
+      obj = fileIn.readObjectFromFile(fileName);
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println("File not found getting");
+    }
+    catch (ClassNotFoundException e)
+    {
+      System.out.println("Class not found getting");
+    }
+    catch (IOException e)
+    {
+      System.out.println("IO exception getting intructors");
+      e.printStackTrace();
+    }
+    return (DateList) obj;
+  }
+
+  public void addInstructor(Date obj)
+  {
+    DateList date = getAllDates();
+    date.addDate(obj);
+    saveDates(date);
+  }
+
+  public void addDates(DateList obj)
+  {
+    DateList date = getAllDates();
+    for (int i = 0; i < obj.size(); i++)
+    {
+      date.addDate(obj.getDate(i));
+    }
+  }
+
+  public void deleteDate(Date date)
+  {
+    DateList dates = getAllDates();
+    dates.removeDate(date);
+    saveDates(dates);
+  }
+
+
+
+
+
+
+}
---
+++ src/saves/WeekFileAdapter.java
@@ -0,0 +1,91 @@
+package saves;
+
+import lists.WeekList;
+import model.Week;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class WeekFileAdapter
+{
+  private String fileName;
+
+  public WeekFileAdapter(String fileName)
+  {
+    this.fileName = fileName;
+  }
+
+  public String getFileName()
+  {
+    return fileName;
+  }
+
+  public void setFileName(String fileName)
+  {
+    this.fileName = fileName;
+  }
+
+  public void saveWeeks(WeekList obj)
+  {
+    FileIO fileOut = new FileIO();
+    try
+    {
+      fileOut.writeToFile(fileName, obj);
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println("File not found");
+    }
+    catch (IOException e)
+    {
+      System.out.println("IO exception saving cars");
+    }
+  }
+
+  public WeekList getAllWeeks()
+  {
+    FileIO fileIn = new FileIO();
+    Object obj = null;
+    try
+    {
+      obj = fileIn.readObjectFromFile(fileName);
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println("File not found getting");
+    }
+    catch (ClassNotFoundException e)
+    {
+      System.out.println("Class not found getting");
+    }
+    catch (IOException e)
+    {
+      System.out.println("IO exception getting intructors");
+      e.printStackTrace();
+    }
+    return (WeekList) obj;
+  }
+
+  public void addWeek(Week obj)
+  {
+    WeekList week = getAllWeeks();
+    week.addWeeks(obj);
+    saveWeeks(week);
+  }
+
+  public void addWeeks(WeekList obj)
+  {
+    WeekList week = getAllWeeks();
+    for (int i = 0; i < obj.size(); i++)
+    {
+      week.addWeeks(obj.getWeek(i));
+    }
+  }
+
+  public void deleteWeek(Week week)
+  {
+    WeekList weeks = getAllWeeks();
+    weeks.removeWeek(week);
+    saveWeeks(weeks);
+  }
+}
